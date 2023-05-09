4a95fa72b558d5857d8ac654ba8115915d5cb5c8
Stella-ugo <nwaiwustellaugo@gmail.com>
2023-05-09 21:19:16 +0100

    Api integration

--- src/index.js
+++ src/index.js
@@ -21,29 +21,54 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Wed", "Thur", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
-                        <div class="weather-forecast-date">${day}</div>
-                        <img src="https://openweathermap.org/img/wn/04d@2x.png" alt="" width="40" />
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+                        <div class="weather-forecast-date">${formatDay(
+                          forecastDay.dt
+                        )}</div>
+                        
+                        <img src="https://openweathermap.org/img/wn/${
+                          forecastDay.weather[0].icon
+                        }@2x.png" alt="" width="40" />
                         <div class="weather-forecast-temperature">
-                            <span class="weather-forecast-temperature-max">18°</span>
-                            <span class="weather-forecast-temperature-min">12°</span>
+                            <span class="weather-forecast-temperature-max">${Math.round(
+                              forecastDay.temp.max
+                            )}°</span>
+                            <span class="weather-forecast-temperature-min">${Math.round(
+                              forecastDay.temp.min
+                            )}°</span>
 
                         </div>
                     </div>
                 `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
 }
+
+function getForecast(coordinates) {
+  let apiKey = "ebef9ca4a8de66ed586fac628fade056";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -63,6 +88,9 @@ function displayTemperature(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -96,4 +124,3 @@ let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
 searchCity("Abuja");
-displayForecast();
